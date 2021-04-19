fddc6b64e870e5015b907a4ba0abff665a8f9d16
Timothy Jones <timothy.l.jones@gmail.com>
2021-04-19 22:01:14 +1000

    Initial commit

---
+++ pact-js-dev-prettier-config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tabWidth: 2, // This is the default anyway
+  semi: true, // This is also the default
+  trailingComma: 'es5', // Another default option
+  singleQuote: true,
+};
